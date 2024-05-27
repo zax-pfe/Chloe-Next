11bdad5b8e0f074e6d557f204ec544d1780765a4
AmaAxel <axel.puech@amaris.com>
2024-05-27 15:50:05 +0200

    fetch test

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,3 +2,17 @@
 
 import prisma from "../lib/prisma";
 import { redirect } from "next/navigation";
+
+export async function fetchArtworksByCategory(categoryId: number) {
+  try {
+    const artworks = await prisma.artwork.findMany({
+      where: {
+        categoryId: categoryId,
+      },
+    });
+    console.log(artworks);
+    return artworks;
+  } catch (error) {
+    console.error("Error fetching artworks:", error);
+  }
+}
--- src/app/gallery/ceramics/page.tsx
+++ src/app/gallery/ceramics/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <p>ceramics</p>;
+import prisma from "@/lib/prisma";
+import * as actions from "@/actions";
+
+export default async function Page() {
+  const artworks = await actions.fetchArtworksByCategory(1);
+  return (
+    <div>
+      {!artworks ? (
+        <div>No artworks found</div>
+      ) : (
+        artworks.map((artwork) => (
+          <div key={artwork.id}>
+            {artwork.name} - {artwork.dimensions}
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
