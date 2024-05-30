698290404a3b4fbdbaec4523b94861e0c6409496
AmaAxel <axel.puech@amaris.com>
2024-05-30 14:56:36 +0200

    updated the page for gallery

--- src/app/gallery/[id]/page.tsx
+++ src/app/gallery/[id]/page.tsx
@@ -1,11 +1,3 @@
-"use client";
-import PageItem from "@/components/pageItem";
-import Link from "next/link";
-import { notFound } from "next/navigation";
-import { motion, AnimatePresence } from "framer-motion";
-import React, { useState, useEffect } from "react";
-import Inner from "@/components/layout/Inner";
-
 interface ArtworksPageItem {
   params: {
     id: number;
@@ -14,27 +6,6 @@ interface ArtworksPageItem {
 
 export default function Page(props: ArtworksPageItem) {
   const index = props.params.id;
-  const [visiblePage, setVisiblePage] = useState(true);
 
-  return (
-    <Inner>
-      <PageItem index={index} setVisiblePage={setVisiblePage} />;
-    </Inner>
-    // <AnimatePresence>
-    //   {visiblePage && (
-    //     <motion.div
-    //       className={`pageItem-container`}
-    //       initial={{ opacity: 0, y: 50 }}
-    //       animate={{ opacity: 1, y: 0 }}
-    //       exit={{ opacity: 0, y: 50 }}
-    //     >
-    //       <div>
-    //         {" "}
-    //         {`artwork : ${index}`}
-    //         <PageItem index={index} setVisiblePage={setVisiblePage} />;
-    //       </div>
-    //     </motion.div>
-    //   )}
-    // </AnimatePresence>
-  );
+  return <div></div>;
 }
