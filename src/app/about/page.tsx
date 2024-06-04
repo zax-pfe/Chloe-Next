6b6d42bdfca0a28f20e8145c8104b68c896a44df
AmaAxel <axel.puech@amaris.com>
2024-06-04 18:29:45 +0200

    adding animation about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,16 @@
-export default async function AboutPage() {
-  return <div>About</div>;
+"use client";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+export default function AboutPage() {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3 }}
+      className="about-container"
+    >
+      a
+    </motion.div>
+  );
 }
