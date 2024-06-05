61613ec257fab178abd313614c512424840d513d
zax-pfe <axel75puech@gmail.com>
2024-06-05 19:08:57 +0200

    scrollbar management improved

--- src/hooks/adjustScrollBar.tsx
+++ src/hooks/adjustScrollBar.tsx
@@ -1,16 +1,42 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const useLockBodyScroll = (device: string) => {
+  const [scrollbarWidth, setScrollbarWidth] = useState(0);
+
   useEffect(() => {
+    // Function to calculate scrollbar width
+    const calculateScrollbarWidth = () => {
+      const outer = document.createElement("div");
+      outer.style.visibility = "hidden";
+      outer.style.overflow = "scroll"; // forcing scrollbar to appear
+      document.body.appendChild(outer);
+
+      const inner = document.createElement("div");
+      outer.appendChild(inner);
+
+      const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
+      // Ensuring the parent node is not null before removing the child
+      if (outer.parentNode !== null) {
+        outer.parentNode.removeChild(outer);
+      }
+      return scrollbarWidth;
+    };
+
     if (device === "desktop") {
-      document.body.style.paddingRight = "17px";
+      const width = calculateScrollbarWidth();
+      setScrollbarWidth(width);
+
+      document.body.style.paddingRight = `${width}px`;
 
       return () => {
         document.body.style.paddingRight = "";
       };
     }
   }, [device]);
+
+  return scrollbarWidth; // Optional: return scrollbar width if needed elsewhere
 };
 
 export default useLockBodyScroll;
